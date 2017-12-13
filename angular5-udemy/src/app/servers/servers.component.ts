0264f4c5ab0e0da664253b0309403b3e00f23f96
Prashant Katiyar <pkbca2007@gmail.com>
2017-12-14 00:47:56 +0530

    Section 2, Lecture 30

--- angular5-udemy/src/app/servers/servers.component.ts
+++ angular5-udemy/src/app/servers/servers.component.ts
@@ -8,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class ServersComponent implements OnInit {
   serverCreationStatus= 'No Server was created.';
   allowNewServer= false;
-  serverName= '';
+  serverName= 'TestServer';
 
   constructor() {
     setTimeout(() => {
